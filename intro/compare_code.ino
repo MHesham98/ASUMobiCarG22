b2465881f242b5e39ebe16a02b606ded439ded08
khaledlooda <36304731+khaledlooda@users.noreply.github.com>
2018-02-19 23:49:59 +0100

    Add files via upload

---
+++ intro/compare_code.ino
@@ -0,0 +1,44 @@
+//pin def
+int led1=13;
+int led2=12;
+int led3=11;
+
+int in1=A0;
+int in2=A1;
+int in3=A2;
+//global
+int i;
+//function of compare
+void compare(int x,int y,int z)
+{
+  if(x>y&&x>z)
+  digitalWrite(led1,1);
+  else if(y>x&&y>z)
+    digitalWrite(led2,1);
+    else
+      digitalWrite(led3,1);
+  }
+  
+void setup()
+{
+  for( i=13;i>=11;i--)
+  {
+  pinMode(i,OUTPUT);
+  }
+}
+void loop()
+{
+  int x=analogRead(in1);
+  int y=analogRead(in2);
+  int z=analogRead(in3);
+  compare(x,y,z);
+  delay(1000);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);
+  digitalWrite(led3,0);
+
+
+
+}
+  
+
