a94da003b9debbfa405af8b6dba985c0770fa440
khaledlooda <36304731+khaledlooda@users.noreply.github.com>
2018-03-07 19:53:08 +0100

    Add files via upload

---
+++ Phase 1 (Easy Driving Application)/Hardware/Testing/bluetooth module/testing_code.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(7,OUTPUT);
+}
+void loop()
+{
+  digitalWrite(13,1);
+  digitalWrite(12,0);
+  analogWrite(11,200);
+  digitalWrite(4,1);
+  digitalWrite(7,0);
+  analogWrite(5,200);
+  
+  
+ }
