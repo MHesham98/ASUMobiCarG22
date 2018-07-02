7d095daf31dd358058b1db8d619cfd7c4f25d676
Khaled Essam <khaled.e13297@gmail.com>
2018-07-02 12:01:12 +0200

    Update code_of_phase_1.ino

--- Final video and code of phase 1/code/code_of_phase_1.ino
+++ Final video and code of phase 1/code/code_of_phase_1.ino
@@ -36,7 +36,73 @@ void loop() {
   
     k=Serial.read();
     if(k== 'F'&& distanceInCms<100);
+    else if(k == 'F')
+    {
+
+      digitalWrite(M1A,HIGH);
+      digitalWrite(M1B,LOW);
+
+      digitalWrite(M2A,LOW);
+      digitalWrite(M2B,HIGH);
+
+
+      digitalWrite(EN1,HIGH);
+      digitalWrite(EN2,HIGH);
+      
+     }
+     else if(k == 'B'){
+      digitalWrite(M1A,LOW);
+      digitalWrite(M1B,HIGH);
+
+      digitalWrite(M2A,HIGH);
+      digitalWrite(M2B,LOW);
+
+
+      digitalWrite(EN1,HIGH);
+      digitalWrite(EN2,HIGH);
+      
+      }
+      else if(k == 'L'){
+      digitalWrite(M1A,LOW);
+      digitalWrite(M1B,LOW);
+
+      digitalWrite(M2A,LOW);
+      digitalWrite(M2B,HIGH);
+
+
+      digitalWrite(EN1,HIGH);
+      digitalWrite(EN2,HIGH);
+      
+      }
+      else if(k == 'R'){
+      digitalWrite(M1A,HIGH);
+      digitalWrite(M1B,LOW);
+
+      digitalWrite(M2A,LOW);
+      digitalWrite(M2B,LOW);
+
+
+      digitalWrite(EN1,HIGH);
+      digitalWrite(EN2,HIGH);
+      
+      }
+      else{
+        
+        digitalWrite(M1A,LOW);
+      digitalWrite(M1B,LOW);
+
+      digitalWrite(M2A,LOW);
+      digitalWrite(M2B,LOW);
+
+
+      digitalWrite(EN1,LOW);
+      digitalWrite(EN2,LOW);
+      
+        }
     
+  }
+
+}
     
     
 void forward()
